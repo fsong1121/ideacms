5a39213844095bd2384a36197476fea75c0e5c85
老范 <245873000@qq.com>
2025-03-17 14:48:03 +0800

    常规更新

--- Site/config/cache.php
+++ Site/config/cache.php
@@ -6,7 +6,7 @@
 
 return [
     // 默认缓存驱动
-    'default' => 'file',
+    'default' => env('CACHE_TYPE', 'file'),
 
     // 缓存连接方式配置
     'stores'  => [
@@ -23,6 +23,14 @@ return [
             'tag_prefix' => 'tag:',
             // 序列化机制 例如 ['serialize', 'unserialize']
             'serialize'  => [],
+        ],
+        'redis' => [
+            'type'     => 'redis',     // 驱动类型
+            'host'     => env('REDIS_HOST', '127.0.0.1'), // Redis服务器IP
+            'port'     => env('REDIS_PORT', '6379'),        // 端口
+            'password' => env('REDIS_PASS', ''),          // 密码（若无密码则留空）
+            'select'   => env('REDIS_SELECT', '1'),           // 默认数据库编号
+            'prefix'   => env('REDIS_PRE', ''),          // 缓存前缀（可选）
         ],
         // 更多的缓存连接
     ],
